ee8c7b470c69cd4d694aa8e3f1eb8e7a6c32ace6
JeffreyKayfes <jeffreykayfes@gmail.com>
2024-11-03 15:31:18 -0600

    Completed Request 407 and 409. Learning Activity Completed

--- SingleResponsibilityPrinciple/URLTradeDataProvider.cs
+++ SingleResponsibilityPrinciple/URLTradeDataProvider.cs
@@ -1,5 +1,6 @@
 ﻿using SingleResponsibilityPrinciple.Contracts;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,13 +10,53 @@ namespace SingleResponsibilityPrinciple
 {
     public class URLTradeDataProvider : ITradeDataProvider
     {
+        string url;
+        ILogger logger;
+
         public URLTradeDataProvider(string url, ILogger logger)
         {
+            this.url = url;
+            this.logger = logger;
+        }
+
+        public async Task<IEnumerable<string>> GetTradeAsync()
+        {
+            List<string> tradeData = new List<string>();
+            logger.LogInfo("Reading trades from URL: " + url);
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync(url);
+                if (!response.IsSuccessStatusCode)
+                {
+                    // Log error and throw an exception if the URL fails
+                    logger.LogWarning($"Failed to retrieve data. Status code: {response.StatusCode}");
+                    throw new Exception($"Error retrieving data from URL: {url}");
+                }
+
+                // Use asynchronous stream reading to access the data
+                using (Stream stream = await response.Content.ReadAsStreamAsync())
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    string line;
+                    while ((line = await reader.ReadLineAsync()) != null)
+                    {
+                        tradeData.Add(line);
+                    }
+                }
+            }
+
+            logger.LogInfo("Successfully read trade data from URL.");
+            return tradeData;
         }
 
         public IEnumerable<string> GetTradeData()
         {
-            throw new NotImplementedException();
+            Task<IEnumerable<string>> task = Task.Run(() => GetTradeAsync());
+            task.Wait();
+
+            IEnumerable<string> tradeList = task.Result;
+            return tradeList;
         }
     }
 }
