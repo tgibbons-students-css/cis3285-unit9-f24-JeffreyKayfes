d353e8dd72beb66c5758bf69a4555225d7c3ff45
JeffreyKayfes <jeffreykayfes@gmail.com>
2024-11-03 16:59:59 -0600

    Request 405 completed

--- SingleResponsibilityPrinciple/ConsoleLogger.cs
+++ SingleResponsibilityPrinciple/ConsoleLogger.cs
@@ -8,12 +8,27 @@ namespace SingleResponsibilityPrinciple
     {
         public void LogWarning(string message, params object[] args)
         {
-            Console.WriteLine(string.Concat("WARN: ", message), args);
+            //Console.WriteLine(string.Concat("WARN: ", message), args);
+            LogMessage("WARN", message, args);
         }
 
         public void LogInfo(string message, params object[] args)
         {
-            Console.WriteLine(string.Concat("INFO: ", message), args);
+            //Console.WriteLine(string.Concat("INFO: ", message), args);
+            LogMessage("INFO", message, args);
+        }
+
+        private void LogMessage(string type, string message, params object[] args)
+        {
+            // Log to console
+            Console.WriteLine($"{type}: {message}", args);
+
+            // Log to XML file
+            using (StreamWriter logfile = File.AppendText("log.xml"))
+            {
+                string formattedMessage = string.Format(message, args);
+                logfile.WriteLine($"<log><type>{type}</type><message>{formattedMessage}</message></log>");
+            }
         }
     }
 }
